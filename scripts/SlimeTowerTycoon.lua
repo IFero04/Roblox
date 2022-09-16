07fe0b2ce6fe392bc6f3145939c97e5c788ab354
Beaast-exe <32130606+Beaast-exe@users.noreply.github.com>
2022-09-16 12:49:16 +0100

    Add files via upload

--- scripts/SlimeTowerTycoon.lua
+++ scripts/SlimeTowerTycoon.lua
@@ -23,14 +23,14 @@ local settings = {
 }
 
 -- // UI SETUP \\ --
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
 
 local Window = OrionLib:MakeWindow({
 	Name = "Beaast Hub",
 	HidePremium = false,
 	SaveConfig = true,
 	ConfigFolder = "BeaastHubSlimeTower",
-	IntroEnabled = true,
+	IntroEnabled = false,
 	IntroText = "Beaast Hub"
 })
 
@@ -55,6 +55,81 @@ Autos:AddToggle({
 	end
 })
 
+Autos:AddToggle({
+	Name = "Enable Auto Deposit",
+	Default = false,
+	Callback = function(bool)
+		settings.autoDeposit = bool
+		if bool then
+			doAutoDeposit()
+		end
+	end
+})
+
+Autos:AddToggle({
+	Name = "Enable Auto Merge",
+	Default = false,
+	Callback = function(bool)
+		settings.autoMerge = bool
+		if bool then
+			doAutoMerge()
+		end
+	end
+})
+
+local Buy = Main:AddSection({
+	Name = 'Buy'
+})
+
+Buy:AddDropdown({
+	Name = "Buy Slime Amount",
+	Default = "1",
+	Options = {"1", "5", "25", "50"},
+	Callback = function(amount)
+		settings.autoBuySlime.amount = amount
+	end
+})
+
+Buy:AddToggle({
+	Name = "Auto Buy Slimes",
+	Default = false,
+	Callback = function(bool)
+		settings.autoBuySlime.enabled = bool
+		if bool then
+			doAutoBuySlime(settings.autoBuySlime.amount)
+		end
+	end
+})
+
+Buy:AddToggle({
+	Name = "Auto Buy Rate",
+	Default = false,
+	Callback = function(bool)
+		settings.autoBuyRate = bool
+		if bool then
+			doAutoBuyRate()
+		end
+	end
+})
+
+-- // MISC TAB \\ --
+local Misc = Window:MakeTab({
+	Name = "Misc",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Misc:AddButton({
+	Name = "Destroy UI",
+	Callback = function()
+      	for i, v in pairs(game.CoreGui:GetDescendants()) do
+		if v.Name == "Orion" then
+			v:Destroy()
+		end
+end
+  	end
+})
+
 -- // CHEAT FUNCTIONS \\ --
 
 function TeleportToMe(item)
@@ -70,21 +145,61 @@ function doAutoCollect()
 			TeleportToMe(v)
 		end
 	end
+
+	local added
+	added = game:GetService("Workspace").Drops.ChildAdded:Connect(function(v)
+		wait(0.2)
+		if v:IsA("Part") and v.Name == "Dropper_Drop" then
+			TeleportToMe(v)
+		end
+	end)
+
+	spawn(function()
+		while task.wait(0.1) do
+			if settings.autoCollect ~= true then
+				added:Disconnect()
+				break;
+			end
+		end
+	end)
 end
 
--- // MISC TAB \\ --
-local Misc = Window:MakeTab({
-	Name = "Misc",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
+function doAutoDeposit()
+	spawn(function()
+		while settings.autoDeposit do
+			events.Deposit:FireServer()
+			wait(0.1)
+		end
+	end)
+end
 
-Misc:AddButton({
-	Name = "Destroy UI",
-	CallBack = function()
-		print("teste")
-	end
-})
+function doAutoBuySlime(value)
+	spawn(function()
+		while settings.autoBuySlime.enabled do
+			local amount = value or settings.autoBuySlime.amount
+			events.Buy_Dropper:FireServer(tonumber(amount))
+			wait(0.1)
+		end
+	end)
+end
+
+function doAutoBuyRate()
+	spawn(function()
+		while settings.autoBuyRate do
+			events.Buy_Speed:FireServer(1)
+			wait(0.1)
+		end
+	end)
+end
+
+function doAutoMerge()
+	spawn(function()
+		while settings.autoMerge do
+			events.Merge:FireServer()
+			wait(0.1)
+		end
+	end)
+end
 
 -- // INITIALIZE THE SCRIPT \\ --
 OrionLib.Init()

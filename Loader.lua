709d7b7fb0e4d2b617af4b14de5d0f7f749f59f3
Beaast-exe <32130606+Beaast-exe@users.noreply.github.com>
2022-09-16 10:43:29 +0100

    Add files via upload

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+repeat wait() until game:IsLoaded()
+local placeID = game.PlaceId
+
+-- // SLIME TOWER TYCOON \\ --
+if placeID == 10675066724 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Beaast-exe/Roblox/master/scripts/SlimeTowerTycoon.lua"))()
+end

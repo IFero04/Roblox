bcd01af62859a808e28e2f874c8cf9d2bf71ad50
Beaast-exe <32130606+Beaast-exe@users.noreply.github.com>
2022-09-18 17:30:07 +0100

    Update BeaastHub.lua

--- BeaastHub.lua
+++ BeaastHub.lua
@@ -4,4 +4,6 @@ local placeID = game.PlaceId
 -- // SLIME TOWER TYCOON \\ --
 if placeID == 10675066724 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Beaast-exe/Roblox/master/scripts/SlimeTowerTycoon.lua"))()
+elseif placeID == 9285238704 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Beaast-exe/Roblox/master/scripts/RaceClicker.lua"))()
 end

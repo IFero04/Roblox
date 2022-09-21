e25b0d9cc013e70cd1078a050871dadd716dd468
Beaast-exe <32130606+Beaast-exe@users.noreply.github.com>
2022-09-21 08:14:33 +0100

    Update BeaastHub.lua

--- BeaastHub.lua
+++ BeaastHub.lua
@@ -14,4 +14,7 @@ if placeID == 10675066724 then
 -- // RACE CLICKER \\ --
 elseif placeID == 9285238704 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Beaast-exe/Roblox/master/scripts/RaceClicker.lua"))()
+-- // BEE SWARM SIMULATOR \\ --
+elseif placeID == 1537690962 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Beaast-exe/Roblox/master/scripts/BeeSwarmSimulator.lua"))()
 end

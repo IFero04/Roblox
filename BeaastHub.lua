2b355405591eccaa8e0b57534a4d9a94e965deed
Beaast-exe <32130606+Beaast-exe@users.noreply.github.com>
2022-09-21 08:05:09 +0100

    Update BeaastHub.lua

--- BeaastHub.lua
+++ BeaastHub.lua
@@ -1,9 +1,17 @@
 repeat wait() until game:IsLoaded()
 local placeID = game.PlaceId
 
+-- // ANTI-AFK \\ --
+local VirtualUser = game:GetService('VirtualUser')
+game:GetService('Players').LocalPlayer.Idled:connect(function()
+	VirtualUser:CaptureController()
+	VirtualUser:ClickButton2(Vector2.new())
+end)
+
 -- // SLIME TOWER TYCOON \\ --
 if placeID == 10675066724 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Beaast-exe/Roblox/master/scripts/SlimeTowerTycoon.lua"))()
+-- // RACE CLICKER \\ --
 elseif placeID == 9285238704 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Beaast-exe/Roblox/master/scripts/RaceClicker.lua"))()
 end

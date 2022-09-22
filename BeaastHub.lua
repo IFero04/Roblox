78265a753305d8774086da923763c39c6634cd0b
Beaast-exe <r.fariaa18@gmail.com>
2022-09-22 09:05:01 +0100

    remove bee swarm simulator

--- BeaastHub.lua
+++ BeaastHub.lua
@@ -1,9 +1,9 @@
-repeat wait() until game:IsLoaded()
+repeat task.wait() until game:IsLoaded()
 local placeID = game.PlaceId
 
 -- // ANTI-AFK \\ --
 local VirtualUser = game:GetService('VirtualUser')
-game:GetService('Players').LocalPlayer.Idled:connect(function()
+game:GetService('Players').LocalPlayer.Idled:Connect(function()
 	VirtualUser:CaptureController()
 	VirtualUser:ClickButton2(Vector2.new())
 end)
@@ -14,7 +14,4 @@ if placeID == 10675066724 then
 -- // RACE CLICKER \\ --
 elseif placeID == 9285238704 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Beaast-exe/Roblox/master/scripts/RaceClicker.lua"))()
--- // BEE SWARM SIMULATOR \\ --
-elseif placeID == 1537690962 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Beaast-exe/Roblox/master/scripts/BeeSwarmSimulator.lua"))()
 end
